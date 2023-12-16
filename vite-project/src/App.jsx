110c19eafd39215993d621135e6005e46e0fa28e
wahyu-dermawan <wahyudermawan399@gmail.com>
2023-12-16 22:12:45 +0700

    add nav and first display

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import carImage from './assets/img_car.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <a className="navbar-brand" href="#">Sewa Mobil</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Ourservice </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#whyus">Why Us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#testimonial">Testimonial</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#faq">FAQ</a>
+              </li>
+              {/* <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+              </li> */}
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section className="jumbotron">
+          <div className="container">
+
+            <div className="row align-items-center"> 
+            <div className="col-lg-6 p-5">
+            <h1 className="display-1">Sewa & Rental Mobil Terbaik di kawasan ( Lokasimu )</h1>
+            <p className="lead mt-4 mb-4">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
+            <a className="btn btn-primary" href="#mulaisewamobil" role="button" style={{ backgroundColor: '#5CB85F',color:'white',borderColor:'#5CB85F'}}>Mulai Sewa Mobil</a>
+            </div>
+
+            <div className="col-lg-4">
+            <img className ="carImg" src={carImage} alt="Car" />
+            </div>
+          
+            </div>
+          </div>
+        </section>
+
+        <section className="container">
+          <h2>Why Us?</h2>
+          <p>Best Car Rental for any kind of in Lakin. Frequently Asked Quest and Testimonial also available.</p>
+        </section>
+      </main>
+      <footer className="text-center">
+        <p>&copy; 2020 Sewa & Rental Mobil. All rights reserved.</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
